8d9e62742e39e88603f67259ff6d749a8e313725
Gustavo <gustavotch@hotmail.com>
2021-04-22 13:45:09 -0300

    Controllers implementation and design modifications

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -1,8 +1,19 @@
-import 'package:DevQuiz/core/app_text_styles.dart';
-import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
+
 class QuestionIndicatorWidget extends StatelessWidget {
+
+  final int currentPage;
+  final int length;
+
+  const QuestionIndicatorWidget({
+    Key? key,
+    required this.currentPage,
+    required this.length,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,16 +23,16 @@ class QuestionIndicatorWidget extends StatelessWidget {
          Row(
            mainAxisAlignment: MainAxisAlignment.spaceBetween,
            children: [
-              Text("Questão 04",
+              Text("Questão $currentPage",
               style: AppTextStyles.body,
               ),
-              Text(" de 10",
+              Text(" de $length",
               style: AppTextStyles.body,
               ),
            ],
          ),
          SizedBox(height: 16),
-         ProgressIndicatorWidget(value: 0.7),
+         ProgressIndicatorWidget(value: currentPage / length),
         ],
       ),
     );
--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,35 +1,48 @@
 import 'package:DevQuiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/answer_model.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
 
-  final String title;
+  final QuestionModel question;
+  final VoidCallback onChange;
 
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+  const QuizWidget({Key? key, required this.question, required this.onChange}) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+
+  int indexSelected = -1;
+
+  AnswerModel anwers(int index) => widget.question.anwers[index];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          Text(title,
+          SizedBox(height: 64),
+          Text(widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(height: 24),
+          for(var i = 0; i < widget.question.anwers.length; i++ )
           AnswerWidget(
-            isSelected: true,
-            isRight: true,
-            title: "Possibilita a criação de aplicativos compilados nativamente",
-            ),
-          AnswerWidget(
-            title: "Possibilita a criação de aplicativos compilados nativamente",
-            ),
-          AnswerWidget(
-            title: "Possibilita a criação de aplicativos compilados nativamente",
-            ),
-          AnswerWidget(
-            title: "Possibilita a criação de aplicativos compilados nativamente",
+            answer: anwers(i),
+            disabled: indexSelected != -1,
+            isSelected: indexSelected == i,
+            onTap: (){
+              
+              indexSelected = i;
+              setState(() {});
+              Future.delayed(Duration(seconds: 1))
+                .then((value) =>  widget.onChange());
+            },
             ),
         ],
       ),
